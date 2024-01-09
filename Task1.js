407226d94100d31e40588691864757cde01afdf4
VALIANTSIN MAKAR <63017329+Val1k@users.noreply.github.com>
2024-01-09 14:00:49 +0300

    end

--- Task1.js
+++ Task1.js
@@ -6,5 +6,4 @@ let hourInDay = 24;
 let secondInDay = secondsInHour * hourInDay;
 let daysInYear = 365;
 let secnodsInYear = daysInYear * secondInDay;
-let age = prompt(how old are you,[]);
-let ageInSecond = age * secnodsInYear;
+let ageInSecond = 23 * secnodsInYear;
